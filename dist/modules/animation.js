132f3eb8d757544df12109c389f0a5f6ce212e00
Ahmed Abdalwahab <ahmed96mahmoud1996@gmail.com>
2022-06-03 20:15:38 +0200

    update build Js Files

--- dist/modules/animation.js
+++ dist/modules/animation.js
@@ -1,31 +1,33 @@
 // Add the animation to the icons and display div
 export const displayAnimation = () => {
-  const display = document.querySelector('.display');
-  const symbols = display.querySelectorAll('.icon');
-  const selectElement = document.querySelector('select');
-  const inputElement = document.querySelector('input');
-  const bar = document.querySelector('#bar');
-  const progress = document.querySelector('#progress');
-  if (!(selectElement.value === '') && !(inputElement.value === '')) {
-    // Make sure that both coutry and zipcode are provided FIRST!
-    if (!symbols[0].classList.contains('fa-beat-fade')) {
-      for (const symbol of symbols) {
-        symbol.classList.toggle('fa-beat-fade');
-      }
-      bar.classList.toggle('hide');
-      bar.classList.toggle('show');
-      progress.classList.toggle('full');
-      // Wait for data to be fetched by the API before continuing with the animation
-      setTimeout(displayAnimation, 4000);
-    } else {
-      for (const symbol of symbols) {
-        symbol.classList.toggle('fa-beat-fade');
-      }
-      bar.classList.toggle('hide');
-      bar.classList.toggle('show');
-      progress.classList.toggle('full');
+    const display = document.querySelector('.display');
+    const symbols = display.querySelectorAll('.icon');
+    const selectElement = document.querySelector('select');
+    const inputElement = document.querySelector('input');
+    const bar = document.querySelector('#bar');
+    const progress = document.querySelector('#progress');
+    if (!(selectElement.value === '') && !(inputElement.value === '')) {
+        // Make sure that both coutry and zipcode are provided FIRST!
+        if (!symbols[0].classList.contains('fa-beat-fade')) {
+            for (const symbol of symbols) {
+                symbol.classList.toggle('fa-beat-fade');
+            }
+            bar.classList.toggle('hide');
+            bar.classList.toggle('show');
+            progress.classList.toggle('full');
+            // Wait for data to be fetched by the API before continuing with the animation
+            setTimeout(displayAnimation, 4000);
+        }
+        else {
+            for (const symbol of symbols) {
+                symbol.classList.toggle('fa-beat-fade');
+            }
+            bar.classList.toggle('hide');
+            bar.classList.toggle('show');
+            progress.classList.toggle('full');
+        }
     }
-  } else {
-    alert('Please make sure to select a country & Enter a zipcode!');
-  }
+    else {
+        alert('Please make sure to select a country & Enter a zipcode!');
+    }
 };
--- dist/modules/update.js
+++ dist/modules/update.js
@@ -1,15 +1,16 @@
 // Defining the update UI function
 export const getAndUpdate = async () => {
-  const request = await fetch('/getdata');
-  try {
-    const retreivedData = await request.json();
-    document.getElementById('cityLoc').textContent = `${retreivedData.loc}`;
-    document.getElementById('date').textContent = `${retreivedData.date}`;
-    document.getElementById('temp').textContent = `${retreivedData.temp} °F`;
-    document.getElementById('windSpeed').textContent = `${retreivedData.wind}`;
-    document.getElementById('humidity').textContent = `${retreivedData.humid}%`;
-    document.getElementById('feel').textContent = `${retreivedData.feel}`;
-  } catch (err) {
-    throw new Error(`Error: ${err}`);
-  }
+    const request = await fetch('/getdata');
+    try {
+        const retreivedData = await request.json();
+        document.getElementById('cityLoc').textContent = `${retreivedData.loc}`;
+        document.getElementById('date').textContent = `${retreivedData.date}`;
+        document.getElementById('temp').textContent = `${retreivedData.temp} °F`;
+        document.getElementById('windSpeed').textContent = `${retreivedData.wind}`;
+        document.getElementById('humidity').textContent = `${retreivedData.humid}%`;
+        document.getElementById('feel').textContent = `${retreivedData.feel}`;
+    }
+    catch (err) {
+        throw new Error(`Error: ${err}`);
+    }
 };
