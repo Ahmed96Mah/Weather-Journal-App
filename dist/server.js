fa1353f612441855feeff0e8ce3a283f762a5a92
Ahmed Abdalwahab <ahmed96mahmoud1996@gmail.com>
2022-06-03 02:23:58 +0200

    update project JS build files

--- dist/server.js
+++ dist/server.js
@@ -5,12 +5,12 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 // Setup empty JS object to act as endpoint for all routes
 const projectData = {
-    temp: '',
-    feel: '',
-    date: '',
-    loc: '',
-    wind: '',
-    humid: '',
+  temp: '',
+  feel: '',
+  date: '',
+  loc: '',
+  wind: '',
+  humid: '',
 };
 // Start up an instance of app
 const app = express();
@@ -27,23 +27,23 @@ const port = 8000;
 // Then the server should start listening at the port defined, and send a staring confirmation message
 const server = app.listen(port, InitiateServer);
 function InitiateServer() {
-    console.log(`Server is running @ port: ${port}`);
+  console.log(`Server is running @ port: ${port}`);
 }
 // Define a GET Route
 app.get('/getdata', function getReq(_req, res) {
-    console.log('GET Request Received..');
-    res.send(projectData);
+  console.log('GET Request Received..');
+  res.send(projectData);
 });
 // Define a POST Route
 app.post('/savedata', function postReq(req, res) {
-    console.log('POST Request Received..');
-    const dataObj = req.body;
-    projectData['temp'] = dataObj.temp;
-    projectData['feel'] = dataObj.feel;
-    projectData['date'] = dataObj.date;
-    projectData['loc'] = dataObj.loc;
-    projectData['wind'] = dataObj.wind;
-    projectData['humid'] = dataObj.humid;
-    console.log('POST Request Has been performed..');
-    res.send(projectData); // Properly Terminating the POST Request
+  console.log('POST Request Received..');
+  const dataObj = req.body;
+  projectData['temp'] = dataObj.temp;
+  projectData['feel'] = dataObj.feel;
+  projectData['date'] = dataObj.date;
+  projectData['loc'] = dataObj.loc;
+  projectData['wind'] = dataObj.wind;
+  projectData['humid'] = dataObj.humid;
+  console.log('POST Request Has been performed..');
+  res.send(projectData); // Properly Terminating the POST Request
 });
