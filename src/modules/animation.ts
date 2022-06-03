2ddf411a5787b6c4ab11cb1329bf21268b4cb4b8
Ahmed Abdalwahab <ahmed96mahmoud1996@gmail.com>
2022-06-03 02:18:27 +0200

    update animation module

--- src/modules/animation.ts
+++ src/modules/animation.ts
@@ -1,63 +1,35 @@
-// Change the layout of the display div from flex-row to flex-column (inherted)
-export const ChangeLayout = (): void => {
-  const display = document.querySelector('.display')!;
-  const divList = display.querySelectorAll('div')!;
-
-  for (const div of divList) {
-    div.classList.toggle('hide');
-  }
-
-  display.classList.toggle('felx-row');
-
-  for (const div of divList) {
-    div.querySelector('p')!.classList.toggle('hide');
-    div.classList.toggle('hide');
-    div.classList.toggle('limitWidth');
-  }
-};
-
-// Extend the display data div
-export const extndDisplay = (): void => {
-  const display = document.querySelector('.display')!;
-
-  if (!display.classList.contains('extend')) {
-    display.classList.toggle('extend');
-    setTimeout(extndDisplay, 2000);
-  } else {
-    ChangeLayout();
-  }
-};
-
 // Add the animation to the icons and display div
 export const displayAnimation = (): void => {
   const display = document.querySelector('.display')!;
-  const divList = display.querySelectorAll('div')!;
+  const symbols = display.querySelectorAll('.icon')!;
   const selectElement = document.querySelector('select')!;
   const inputElement = document.querySelector('input')!;
+  const bar = document.querySelector('#bar')!;
+  const progress = document.querySelector('#progress')!;
 
-  if (!divList[0].querySelector('i')!.classList.contains('fa-beat-fade')) {
-    for (const div of divList) {
-      div.querySelector('i')!.classList.toggle('fa-beat-fade');
-    }
-    // Wait for data to be fetched by the API before continuing with the animation
-    if (display.classList.contains('felx-row')) {
-      // Set a reasonable time FOR the first iteration
-      setTimeout(displayAnimation, 5000);
+  if (!(selectElement.value === '') && !(inputElement.value === '')) {
+    // Make sure that both coutry and zipcode are provided FIRST!
+    if (!symbols[0].classList.contains('fa-beat-fade')) {
+      for (const symbol of symbols) {
+        symbol.classList.toggle('fa-beat-fade');
+      }
+
+      bar.classList.toggle('hide');
+      bar.classList.toggle('show');
+      progress.classList.toggle('full');
+
+      // Wait for data to be fetched by the API before continuing with the animation
+      setTimeout(displayAnimation, 4000);
     } else {
-      // Set a lower delay AFTER the 1st iteration
-      setTimeout(displayAnimation, 1000);
+      for (const symbol of symbols) {
+        symbol.classList.toggle('fa-beat-fade');
+      }
+
+      bar.classList.toggle('hide');
+      bar.classList.toggle('show');
+      progress.classList.toggle('full');
     }
   } else {
-    for (const div of divList) {
-      div.querySelector('i')!.classList.toggle('fa-beat-fade');
-    }
-    if (!(selectElement.value === '') && !(inputElement.value === '')) {
-      // Determine if this is the 1st iteration of the application
-      if (display.classList.contains('felx-row')) {
-        extndDisplay(); // Only Extend display div @ 1st iteration
-      }
-    } else {
-      alert('Please make sure to select a country & Enter a zipcode!');
-    }
+    alert('Please make sure to select a country & Enter a zipcode!');
   }
 };
