92522f4019f362f08a794d6f623c194caeb31306
Ahmed Abdalwahab <ahmed96mahmoud1996@gmail.com>
2022-06-08 15:50:44 +0200

    update project JS build files

--- dist/modules/app.js
+++ dist/modules/app.js
@@ -1,9 +1,8 @@
 import { getApiData } from './call.js';
 import { getAndUpdate } from './update.js';
-import { displayAnimation } from './animation.js';
 /* API URL */
 export const baseURL = 'https://api.openweathermap.org/data/2.5/weather?zip=';
-export const apiKey = `&appid=<Your-API-Key-Goes-HERE>&units=imperial`;
+export const apiKey = `&appid=<Your-API-Key-Goes-Here>&units=imperial`;
 // Define the POST Async function
 const postData = async (url = '', data = {}) => {
     const response = await fetch(url, {
@@ -24,6 +23,18 @@ const postData = async (url = '', data = {}) => {
         throw new Error(`Error: ${err}`);
     }
 };
+const setDate_Time = () => {
+    const time_parag = document.getElementById('time');
+    const date_parag = document.getElementById('date');
+    // Create a new date instance dynamically with JS
+    const date = new Date();
+    const dateStr = date.toString();
+    const time = `${dateStr.slice(16, 21)}`;
+    const fullDate = `${dateStr.slice(0, 3)}, ${dateStr.slice(4, 7)} ${dateStr.slice(8, 10)}, ${dateStr.slice(11, 15)}`;
+    date_parag.textContent = fullDate;
+    time_parag.textContent = time;
+    setInterval(setDate_Time, 60000);
+};
 // Define the main function that runs after the DOM has successfuly loaded
 const init = () => {
     // Defining The social animation listener Callback
@@ -34,12 +45,10 @@ const init = () => {
             evt.target.classList.toggle('fa-style');
         }
     };
+    // Set date and time for the current page
+    setDate_Time();
     // Defining The listener Callback
     const processClick = () => {
-        // Create a new date instance dynamically with JS
-        const d = new Date();
-        const newDate = d.getMonth() + 1 + '.' + d.getDate() + '.' + d.getFullYear();
-        displayAnimation();
         // First, collect user's zipcode, feeling & country selection
         const zipCode = document.getElementById('zipcode')
             .value;
@@ -57,7 +66,6 @@ const init = () => {
                     //@ts-ignore
                     temp: data['main'].temp,
                     feel: userFeel,
-                    date: newDate,
                     loc: data['name'],
                     humid: data['main'].humidity,
                     wind: data['wind'].speed,
@@ -66,15 +74,17 @@ const init = () => {
                 .then(() => {
                 /* There isn't an instance of 'data' here because 'getAndUpdate()' doesen't need it, as it already has a
                  *  defined GET route declared inside of it that retreive the data object from the server.
-                 */
-                setTimeout(() => {
-                    getAndUpdate();
-                }, 3500);
+                */
+                getAndUpdate();
             });
+        }
+        else {
+            alert('Please make sure to select a country & Enter a zipcode!');
         }
     };
     // Adding a click event listener for the button.
     document.getElementById('generate').addEventListener('click', processClick);
+    // Adding Mouse-Related events for social icons
     const socialIcons = document.querySelectorAll('i.fa-brands');
     for (const icon of socialIcons) {
         icon.addEventListener('mouseenter', socialAnimation);
