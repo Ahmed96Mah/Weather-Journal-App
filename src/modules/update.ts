e3337f056c051f1e2190ce4afcad849b517e3fa9
Ahmed Abdalwahab <ahmed96mahmoud1996@gmail.com>
2022-06-08 15:48:06 +0200

    update getandupdate function logic

--- src/modules/update.ts
+++ src/modules/update.ts
@@ -1,17 +1,76 @@
 // Defining the update UI function
 export const getAndUpdate = async (): Promise<void> => {
+  const valueSecs = document.querySelector('.section')!.querySelectorAll('.value');
   const request = await fetch('/getdata');
   try {
     const retreivedData = await request.json();
 
-    document.getElementById('cityLoc')!.textContent = `${retreivedData.loc}`;
-    document.getElementById('date')!.textContent = `${retreivedData.date}`;
-    document.getElementById('temp')!.textContent = `${retreivedData.temp} °F`;
-    document.getElementById('windSpeed')!.textContent = `${retreivedData.wind}`;
-    document.getElementById(
-      'humidity'
-    )!.textContent = `${retreivedData.humid}%`;
-    document.getElementById('feel')!.textContent = `${retreivedData.feel}`;
+    for (const sec of valueSecs) {
+      console.log('next');
+      switch (sec.id) {
+        case 'cityLoc':
+          if (sec.textContent === '') { // If this is the 1st Iteration
+            sec.classList.toggle('hide');
+            sec.textContent = `${retreivedData.loc}`;
+          }else { // If this is 2nd & onward
+            sec.classList.toggle('hide');
+            setTimeout(() => {
+              sec.textContent = `${retreivedData.loc}`;
+              sec.classList.toggle('hide');
+            }, 3000);
+          }
+          break;
+        case 'temp':
+          if (sec.textContent === '') { // If this is the 1st Iteration
+            sec.classList.toggle('hide');
+            sec.textContent = `${retreivedData.temp} °F`;
+          } else { // If this is 2nd & onward
+            sec.classList.toggle('hide');
+            setTimeout(() => {
+              sec.textContent = `${retreivedData.temp} °F`;
+              sec.classList.toggle('hide');
+            }, 3000);
+          }
+          break;
+        case 'windSpeed':
+          if (sec.textContent === '') { // If this is the 1st Iteration
+            sec.classList.toggle('hide');
+            sec.textContent = `${retreivedData.wind}`;
+          } else { // If this is 2nd & onward
+            sec.classList.toggle('hide');
+            setTimeout(() => {
+              sec.textContent = `${retreivedData.wind}`;
+              sec.classList.toggle('hide');
+            }, 3000);
+          }
+          break;
+        case 'humidity':
+          if (sec.textContent === '') { // If this is the 1st Iteration
+            sec.classList.toggle('hide');
+            sec.textContent = `${retreivedData.humid}%`;
+          } else { // If this is 2nd & onward
+            sec.classList.toggle('hide');
+            setTimeout(() => {
+              sec.textContent = `${retreivedData.humid}%`;
+              sec.classList.toggle('hide');
+            }, 3000);
+          }
+          break;
+        case 'feel':
+          if (sec.textContent === '') { // If this is the 1st Iteration
+            sec.classList.toggle('hide');
+            sec.textContent = `${retreivedData.feel}`;
+          } else { // If this is 2nd & onward
+            sec.classList.toggle('hide');
+            setTimeout(() => {
+              sec.textContent = `${retreivedData.feel}`;
+              sec.classList.toggle('hide');
+            }, 3000);
+          }
+          break;
+        default:
+      }
+    }
   } catch (err) {
     throw new Error(`Error: ${err}`);
   }
