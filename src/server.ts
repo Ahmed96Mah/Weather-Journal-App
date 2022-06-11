2046bfc9ff8169ed05c032a224a208c026d7b9d8
Ahmed Mahmoud Abdalwahab <ahmed96mahmoud1996@gmail.com>
2022-06-11 22:34:14 +0200

    update server TS file
    seperated the endpoints into the handlers folder

--- src/server.ts
+++ src/server.ts
@@ -3,19 +3,11 @@ import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 // Cors for cross origin allowance
 import cors from 'cors';
-
-// Setup empty JS object to act as endpoint for all routes
-const projectData = {
-  temp: '',
-  feel: '',
-  date: '',
-  loc: '',
-  wind: '',
-  humid: '',
-};
+import { weatherRoutes } from './handlers/weather.js';
+import { dashboardRoutes } from './handlers/dashboards.js';
 
 // Start up an instance of app
-const app = express();
+const app: express.Application = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -26,42 +18,14 @@ app.use(cors());
 // Initialize the main project folder
 app.use(express.static('./'));
 
+weatherRoutes(app);
+dashboardRoutes(app);
+
 // Setup Server
-// First we define the port
+// First, we define the port
 const port = 8000;
 
 // Then the server should start listening at the port defined, and send a staring confirmation message
-const server = app.listen(port, InitiateServer);
-
-function InitiateServer() {
+const server = app.listen(port, () => {
   console.log(`Server is running @ port: ${port}`);
-}
-
-// Define a GET Route
-app.get('/getdata', function getReq(_req: Request, res: Response) {
-  console.log('GET Request Received..');
-  res.send(projectData);
-});
-
-type dataObj = {
-  temp: string;
-  feel: string;
-  date: string;
-  loc: string;
-  wind: string;
-  humid: string;
-};
-
-// Define a POST Route
-app.post('/savedata', function postReq(req: Request, res: Response) {
-  console.log('POST Request Received..');
-  const dataObj = req.body as dataObj;
-  projectData['temp'] = dataObj.temp;
-  projectData['feel'] = dataObj.feel;
-  projectData['date'] = dataObj.date;
-  projectData['loc'] = dataObj.loc;
-  projectData['wind'] = dataObj.wind;
-  projectData['humid'] = dataObj.humid;
-  console.log('POST Request Has been performed..');
-  res.send(projectData); // Properly Terminating the POST Request
 });
