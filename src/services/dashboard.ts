3351e20c5fe55ba2802b268e794e97993a22015d
Ahmed Mahmoud Abdalwahab <ahmed96mahmoud1996@gmail.com>
2022-06-11 22:29:06 +0200

    add dashboard TS file

---
+++ src/services/dashboard.ts
@@ -0,0 +1,18 @@
+import Client from '../database.js';
+import { Weather } from '../models/weatherUnit.js';
+
+export class weatherDashboard {
+
+  async lastReading(): Promise<Weather> {
+    try {
+      const conn = await Client.connect();
+      const sql = 'SELECT * FROM weather ORDER BY id DESC LIMIT 1';
+      const lastRead = await conn.query(sql);
+      conn.release();
+      return lastRead.rows[0];
+    } catch (err) {
+      throw new Error(`Error, ${err}`);
+    }
+  }
+  
+}
